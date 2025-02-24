14cef433efc02f86ba94bfa4ec8ac2306c61e3ec
Janis Kovalevskis <janis.kovalevskis@gmail.com>
2025-02-24 18:47:56 +0200

    Docker file cleanup and documentation. Build script template file added

---
+++ containers/buildWindows.docker/start_build.ps1
@@ -0,0 +1,9 @@
+# (c) ArrangoDB, 2025
+
+Write-Output "ArrangoDB build latest started!"
+
+git clone --recurse-submodules --jobs 8 https://github.com/arangodb/arangodb
+
+cmake --fresh -G "Visual Studio 17 2022" -B ./build-arangodb/ -S ./arangodb/  --preset community -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_EXE_LINKER_FLAGS="-fuse-ld=lld-link" -DSKIP_PACKAGING=TRUE -DUSE_JEMALLOC=FALSE -DUSE_LIBUNWIND=FALSE
+
+cmake --build ./build-arangodb/ --target arangod
